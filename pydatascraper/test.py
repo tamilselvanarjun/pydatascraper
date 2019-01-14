e65acc73f19932e395f71d6c130fe9074b3265fc
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2019-01-15 01:02:00 +0530

    updated test case with doc strings

--- pydatascraper/test.py
+++ pydatascraper/test.py
@@ -3,7 +3,10 @@ from pyscraper import get_options, locations
 
 class TestGetOptions(unittest.TestCase):
     def test_get_options_with_valid_input(self):
-        """Test get_options function with valid input."""
+        """Test get_options function with valid input.
+
+        This test case checks the behavior of the get_options function when provided with valid input parameters.
+        """
         scrambled = "etcbat"
         flag = False
         totals = []
@@ -12,7 +15,10 @@ class TestGetOptions(unittest.TestCase):
         self.assertEqual(result, ['etc', 'bat'])
 
     def test_get_options_with_invalid_input(self):
-        """Test get_options function with invalid input."""
+        """Test get_options function with invalid input.
+
+        This test case checks the behavior of the get_options function when provided with invalid input parameters.
+        """
         scrambled = "xyz"
         flag = False
         totals = []
@@ -22,7 +28,10 @@ class TestGetOptions(unittest.TestCase):
 
 class TestLocations(unittest.TestCase):
     def test_child_tree1_with_valid_input(self):
-        """Test child_tree1 function with valid input."""
+        """Test child_tree1 function with valid input.
+
+        This test case checks the behavior of the child_tree1 function when provided with valid input parameters.
+        """
         url = "https://www.marianilandscape.com/where-we-are/"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
@@ -33,7 +42,10 @@ class TestLocations(unittest.TestCase):
         self.assertGreater(len(result), 0)
 
     def test_child_tree2_with_valid_input(self):
-        """Test child_tree2 function with valid input."""
+        """Test child_tree2 function with valid input.
+
+        This test case checks the behavior of the child_tree2 function when provided with valid input parameters.
+        """
         url = "https://landscapedevelopment.com/contact/"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
@@ -44,7 +56,10 @@ class TestLocations(unittest.TestCase):
         self.assertGreater(len(result), 0)
 
     def test_child_tree3_with_valid_input(self):
-        """Test child_tree3 function with valid input."""
+        """Test child_tree3 function with valid input.
+
+        This test case checks the behavior of the child_tree3 function when provided with valid input parameters.
+        """
         url = "https://www.yellowstonelandscape.com/locations"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
