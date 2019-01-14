226c1062092c6b0763a0b61d93a98c911bf29530
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2019-01-15 01:02:00 +0530

    updated test case with doc strings

--- pydatascraper/test.py
+++ pydatascraper/test.py
@@ -3,6 +3,7 @@ from pyscraper import get_options, locations
 
 class TestGetOptions(unittest.TestCase):
     def test_get_options_with_valid_input(self):
+        """Test get_options function with valid input."""
         scrambled = "etcbat"
         flag = False
         totals = []
@@ -11,6 +12,7 @@ class TestGetOptions(unittest.TestCase):
         self.assertEqual(result, ['etc', 'bat'])
 
     def test_get_options_with_invalid_input(self):
+        """Test get_options function with invalid input."""
         scrambled = "xyz"
         flag = False
         totals = []
@@ -20,6 +22,7 @@ class TestGetOptions(unittest.TestCase):
 
 class TestLocations(unittest.TestCase):
     def test_child_tree1_with_valid_input(self):
+        """Test child_tree1 function with valid input."""
         url = "https://www.marianilandscape.com/where-we-are/"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
@@ -30,6 +33,7 @@ class TestLocations(unittest.TestCase):
         self.assertGreater(len(result), 0)
 
     def test_child_tree2_with_valid_input(self):
+        """Test child_tree2 function with valid input."""
         url = "https://landscapedevelopment.com/contact/"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
@@ -40,6 +44,7 @@ class TestLocations(unittest.TestCase):
         self.assertGreater(len(result), 0)
 
     def test_child_tree3_with_valid_input(self):
+        """Test child_tree3 function with valid input."""
         url = "https://www.yellowstonelandscape.com/locations"
         # Replace get_soup_function with actual function to get soup object
         soup = get_soup_function(url)  
