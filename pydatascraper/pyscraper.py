8367cd603ae48eecab6818aa6d2359a3591eef96
tamilselvanarjun <nishantamil@gmail.com>
2021-01-09 10:18:20 +0500

    fixing the bugs

--- pydatascraper/pyscraper.py
+++ pydatascraper/pyscraper.py
@@ -325,7 +325,7 @@ class locations:
                     try:
                         if all_div.h3.text in service and all_div.h3.text not in adder:
                             #adder.append(all_div.h3.text)
-                            #print(list(zip([all_div.h3.text], [all_div.p.text])))
+                            #print(list(zip([all_div.h3.text], [all_div.p.text])))S
                             #df = pd.DataFrame(list(zip([all_div.h3.text], [all_div.p.text])) ,columns = cols)
                             sample_text = all_div.p.text
                             address = sample_text.split('\n')[0].split(',')[0]
@@ -345,9 +345,7 @@ class locations:
                 if h4 in all_tags:
                     try:
                         if all_div.h4.text in service and all_div.h4.text not in adder:
-                            #adder.append(all_div.h4.text)
-                            #print(list(zip([all_div.h4.text], [all_div.p.text])))
-                            #df = pd.DataFrame(list(zip([all_div.h4.text], [all_div.p.text])) ,columns = cols)
+                            
                             sample_text = all_div.p.text
                             address = sample_text.split('\n')[0].split(',')[0]
                             textlabel = sample_text.split('\n')[0].split(',')[1].split('P:')[0]
