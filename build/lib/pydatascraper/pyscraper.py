9c55255a76532befedbf0d9a05520729c3c9f68a
brownolivia34 <yuqdlfzua@hotmail.com>
2023-01-01 10:18:20 +0500

    Optimizing the code

--- build/lib/pydatascraper/pyscraper.py
+++ build/lib/pydatascraper/pyscraper.py
@@ -282,9 +282,7 @@ class locations:
                 if h1 in all_tags:
                     try:
                         if all_div.h1.text in service and all_div.h1.text not in adder:
-                            #adder.append(all_div.h1.text)
-                            #print(list(zip([all_div.h1.text], [all_div.p.text])))
-                            #df = pd.DataFrame(list(zip([all_div.h1.text], [all_div.p.text])) ,columns = cols)
+                           
                             sample_text = all_div.p.text
                             address = sample_text.split('\n')[0].split(',')[0]
                             textlabel = sample_text.split('\n')[0].split(',')[1].split('P:')[0]
