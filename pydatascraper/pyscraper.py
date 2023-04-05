cce71cb1dfc05ca7a961e979d2062ed15e97e2b9
brandonwilliams24 <twkiwxvjunzl@hotmail.com>
2023-04-05 10:18:20 +0500

    updates in code

--- pydatascraper/pyscraper.py
+++ pydatascraper/pyscraper.py
@@ -320,9 +320,7 @@ class locations:
                 if h3 in all_tags:
                     try:
                         if all_div.h3.text in service and all_div.h3.text not in adder:
-                            #adder.append(all_div.h3.text)
-                            #print(list(zip([all_div.h3.text], [all_div.p.text])))S
-                            #df = pd.DataFrame(list(zip([all_div.h3.text], [all_div.p.text])) ,columns = cols)
+                           
                             sample_text = all_div.p.text
                             address = sample_text.split('\n')[0].split(',')[0]
                             textlabel = sample_text.split('\n')[0].split(',')[1].split('P:')[0]
